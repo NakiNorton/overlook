94a18826323048d280930bc06d578809587ea02c
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-30 12:02:52 -0600

    Create function to get all hotel data from fetch request and add to hotel object

--- src/index.js
+++ src/index.js
@@ -4,8 +4,37 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import apiFetch from './apiFetch';
+import hotel from './Hotel'
+// import Room from './Room'
+// import Booking from './Booking'
+// import Guest from './Guest'
+import moment from 'moment';
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// Global variables //
+let todaysDate = moment().format('YYYY/MM/DD');
+
+
+function fetchAllHotelData() {
+  return Promise.all([
+    apiFetch.getAllGuestData(),
+    apiFetch.getAllBookingsData(),
+    apiFetch.getAllRoomsData(),
+  ])
+  .then((data) => {
+     return data;
+  })
+  .then((data) => {
+    hotel.defineHotelData(data[0], data[1], data[2], todaysDate)
+    console.log('hotel', hotel)
+  })  
+  
+}
+
+
+// on window load//
+window.addEventListener('load', fetchAllHotelData)
