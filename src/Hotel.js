d74251a22eb6600fe8fa3f69e457e928b32b7cbe
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-31 08:45:54 -0600

    Write tests and implementation code for hotel class

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,18 +1,49 @@
-const hotel = {
-  guests: null,
-  bookings: null,
-  rooms: null,
-  todaysDate: null,
-  
-  defineHotelData(guests, bookings, rooms, date) {
-    this.guests = guests;
-    this.bookings = bookings;
-    this.rooms = rooms;
-    this.todaysDate = date;
-    }
+class Hotel {
+  constructor(guests, bookings, rooms, date) {
+  this.guests = guests;
+  this.bookings = bookings;
+  this.rooms = rooms;
+  this.todaysDate = date;
+  this.availableRooms = [];
+  this.todaysRevenue = [];
 }
 
-export default hotel;
+  addRoomInfoToBookings() {
+    this.bookings.map(booking => {
+      this.rooms.forEach(room => {
+        if (room.number === booking.roomNumber) {
+          booking.roomType = room.roomType,
+          booking.bidet = room.bidet,
+          booking.bedSize = room.bedSize,
+          booking.numBeds = room.numBeds,
+          booking.costPerNight = room.costPerNight
+        }
+      })
+      this.bookings.push(booking)
+    })
+  };
+
+  getAvailableRooms(date = this.todaysDate) {
+    let availRooms = this.bookings.filter(booking => booking.date !== date)
+    
+    this.rooms.filter(room => {
+      availRooms.forEach(bookedRoom => {
+        if (room.number === bookedRoom.roomNumber) {
+          console.log(room)
+          return this.availableRooms.push(room) 
+        }
+      })
+    })
+  }
+
+  // calculateTodaysRevenue() {
+
+  // }
+
+
+}
+
+export default Hotel;
 
 
 
