d0445e80ddef1d03229570649e9ed9b7b66796ae
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-31 11:10:23 -0600

    Finish writing hotel methods for tdd and refactor

--- src/Hotel.js
+++ src/Hotel.js
@@ -5,7 +5,7 @@ class Hotel {
   this.rooms = rooms;
   this.todaysDate = date;
   this.availableRooms = [];
-  this.todaysRevenue = null;
+  this.bookedRooms = [];
 }
 
   addRoomInfoToBookings() {
@@ -20,25 +20,39 @@ class Hotel {
         }
       })
     })
-
   };
 
-  getAvailableRooms(date = this.todaysDate) {
+  findAvailableRooms(date = this.todaysDate) {
     let availRooms = this.bookings.filter(booking => booking.date !== date)
-    
     this.rooms.filter(room => {
-      availRooms.forEach(bookedRoom => {
-        if (room.number === bookedRoom.roomNumber) {
-          console.log(room)
+      availRooms.forEach(availRoom => {
+        if (room.number === availRoom.roomNumber) {
           return this.availableRooms.push(room) 
         }
       })
     })
   }
 
-  // calculateTodaysRevenue() {
+  findBookedRooms(date = this.todaysDate) {
+    let unavailRooms = this.bookings.filter(booking => booking.date === date)
+    this.rooms.filter(room => {
+      unavailRooms.forEach(bookedRoom => {
+        if (room.number === bookedRoom.roomNumber) {
+          return this.bookedRooms.push(room)
+        }
+      })
+    })
+  }
 
-  // }
+  calculateTodaysRevenue(date) {
+    this.addRoomInfoToBookings()
+    let todaysBookings = this.bookings.filter(booking => booking.date === date)
+    let rev = todaysBookings.reduce((total, booking) => {
+      return total + booking.costPerNight
+    }, 0)
+    console.log(rev)
+    return rev;
+  }
 
 
 }
