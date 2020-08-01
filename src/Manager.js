56f6f331d28504314443e97004c3d3eae34e4d7a
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-01 11:40:42 -0600

    Change hotel class to manager class

--- src/Manager.js
+++ src/Manager.js
@@ -1,7 +1,56 @@
-class Manager {
-  constructor(name) {
-    this.name = name;
+import Guest from './Guest';
+
+class Manager extends Guest {
+  constructor(allGuests, allBookings, allRooms, date, id, name, guestBookings) {
+    super(id, name, guestBookings)
+    this.allGuests = allGuests;
+    this.allBookings = allBookings;
+    this.allRooms = allRooms;
+    this.todaysDate = date;
+    this.availableRooms = [];
+    this.bookedRooms = [];
+}
+
+  findAvailableRooms(date = this.todaysDate) {
+    let availRooms = this.allBookings.filter(booking => booking.date !== date)
+    this.allRooms.filter(room => {
+      availRooms.forEach(availRoom => {
+        if (room.number === availRoom.roomNumber) {
+          return this.availableRooms.push(room) 
+        }
+      })
+    })
   }
+
+  findBookedRooms(date = this.todaysDate) {
+    let unavailRooms = this.allBookings.filter(booking => booking.date === date)
+    this.allRooms.filter(room => {
+      unavailRooms.forEach(bookedRoom => {
+        if (room.number === bookedRoom.roomNumber) {
+          return this.bookedRooms.push(room)
+        }
+      })
+    })
+  }
+
+  calculateTodaysRevenue(date) {
+    this.addRoomInfoToBookings()
+    let todaysBookings = this.allBookings.filter(booking => booking.date === date)
+    return todaysBookings.reduce((total, booking) => {
+      return total + booking.costPerNight
+    }, 0)
+  }
+
+
+  findGuestBookings() {
+    this.addRoomInfoToBookings()
+    return this.allBookings.filter(booking => booking.userID === this.guestId)
+  }
+
+
 }
 
 export default Manager;
+
+
+
