64c752303efc401de688afd9a1fddcb512b03ee8
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-01 22:25:28 -0600

    Add functionality to display a guests bookings

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -8,15 +8,38 @@ let domUpdates = {
   // },
 
   displayGuestDashboard(guest) {
-    
     let loginForm = document.querySelector('.login-form-container')
     loginForm.classList.add('hide')
     let guestForm = document.querySelector('.guest-dashboard')
     guestForm.classList.remove('hide')
     document.querySelector('.guest-name').innerText = `${guest.getGuestFirstName()}`
-    document.querySelector('.guest-money-spent').innerText = `Total amount spent: ${guest.getTotalCostOfRooms()}`
-   
+    document.querySelector('.guest-money-spent').insertAdjacentHTML('beforeend', `${guest.getTotalCostOfRooms()}`)
+    this.displayGuestBookings(guest)
   },
+
+  displayGuestBookings(guest) {
+    const guestBookings = guest.guestBookings;
+    console.log('guest bookings', guestBookings)
+    guestBookings.forEach(booking => {
+      document.querySelector('.guest-bookings-container').insertAdjacentHTML('beforeend',
+        `<p>Reservation Date: ${booking.date}</p>
+        <p>Reservation Confirmation: ${booking.id}</p>
+        <p>Room Type: ${booking.roomType}</p>
+        <p>Room Number: ${booking.roomNumber}</p>
+        <p>Bed Size: ${booking.bedSize}</p>
+        <p>Beds: ${booking.numBeds}</p>
+        <p>Bidet: ${booking.bidet}</p>
+        <p>Cost Per Night: ${booking.costPerNight}</p>
+        <div class='card-line'></div>
+      `)
+    })
+  },
+
+  // displayAvailableRooms() {
+  
+  // },
+
+  
 
   displayManagerDashboard() {
     let loginForm = document.querySelector('.login-form-container')
