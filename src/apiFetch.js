9b13a3b71679a41f03ce186081c39ec740de43aa
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-04 19:45:19 -0600

    Fix indentation errors

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,24 +1,24 @@
 let apiFetch = {
-    getAllGuestData() {
+  getAllGuestData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
-    .then(response => response.json())
-    .catch(err => console.log(err.message))
-    },
-
-    getAllBookingsData() {
-      return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`)
       .then(response => response.json())
       .catch(err => console.log(err.message))
-    },
+  },
 
-    getAllRoomsData() {
-      return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms`)
-        .then(response => response.json())
-        .catch(err => console.log(err.message))
-    },
+  getAllBookingsData() {
+    return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`)
+      .then(response => response.json())
+      .catch(err => console.log(err.message))
+  },
 
-    postNewBooking(booking) {
-      return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`, {
+  getAllRoomsData() {
+    return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms`)
+      .then(response => response.json())
+      .catch(err => console.log(err.message))
+  },
+
+  postNewBooking(booking) {
+    return fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
